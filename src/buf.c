85c5f6b82c9b0b6bc5b0664a652ad9b958ceb047
RunThem <iccy.fun@outlook.com>
2023-06-08 16:43:21 +0800

    添加缓冲push, pop, peek等函数

--- src/buf.c
+++ src/buf.c
@@ -51,3 +51,86 @@ int buf_init(buf_t* buf, size_t len) {
 err:
   return -1;
 }
+
+bool buf_empty(buf_t* buf) {
+  u_ret_if(buf == nullptr, -1);
+  u_ret_if(_buf == nullptr, -1);
+
+  return _buf->len == 0;
+}
+
+int buf_resize(buf_t* buf, size_t len) {
+  u_ret_if(buf == nullptr, -1);
+  u_ret_if(_buf == nullptr, -1);
+  u_ret_if(len <= _buf->cap, -1);
+
+  _buf = (buf_t)u_realloc(_buf, len);
+  u_alloc_if(_buf);
+
+  _buf->cap = len;
+
+  return 0;
+
+err:
+  return -1;
+}
+
+void buf_clear(buf_t* buf) {
+  u_ret_no_if(buf == nullptr);
+  u_ret_no_if(_buf == nullptr);
+
+  _buf->len      = 0;
+  _buf->c_buf[0] = '\0';
+}
+
+void buf_cleanup(buf_t* buf) {
+  u_ret_no_if(buf == nullptr);
+  u_ret_no_if(_buf == nullptr);
+
+  u_free(_buf);
+  _buf = nullptr;
+}
+
+int __buf_push(buf_t* buf, c_buf c_buffer, size_t size) {
+  int ret = 0;
+
+  u_ret_if(buf == nullptr, -1);
+  u_ret_if(_buf == nullptr, -1);
+  u_ret_if(c_buffer == nullptr, -1);
+
+  if (size > _buf->cap - _buf->len) {
+    ret = buf_resize(buf, buf_cap(_buf->len) + size);
+    u_goto_if(ret != 0);
+  }
+
+  memcpy(&_buf->c_buf[_buf->len], c_buffer, size);
+  _buf->len += size;
+
+  return 0;
+
+err:
+  return -1;
+}
+
+int __buf_pop(buf_t* buf, c_buf c_buffer, size_t size) {
+  u_ret_if(buf == nullptr, -1);
+  u_ret_if(_buf == nullptr, -1);
+  u_ret_if(c_buffer == nullptr, -1);
+  u_ret_if(size == 0 || size > _buf->len, -1);
+
+  memcpy(c_buffer, &_buf->c_buf[_buf->len - size], size);
+  _buf->len -= size;
+
+  return 0;
+}
+
+int __buf_peek(buf_t* buf, c_buf c_buffer, size_t size) {
+  u_ret_if(buf == nullptr, -1);
+  u_ret_if(_buf == nullptr, -1);
+  u_ret_if(c_buffer == nullptr, -1);
+  u_ret_if(size == 0 || size > _buf->len, -1);
+
+  memcpy(c_buffer, &_buf->c_buf[_buf->len - size], size);
+
+  return 0;
+}
--- src/buf.h
+++ src/buf.h
@@ -16,7 +16,7 @@ typedef struct __buf* buf_t;
  *
  * code:
  *    int a[] = { 0x01, 0x05, 0x03 };
- *    buf_t buf = buf_new(a, 10);
+ *    buf_t buf = buf_new(p(a), 10);
  *
  *    // buf -> (10, 32) { 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x03, 0x00};
  * */
@@ -33,3 +33,149 @@ buf_t buf_new(c_buf c_buffer, size_t len);
  *    // buf -> (0, 100) { }
  * */
 int buf_init(buf_t* buf, size_t len);
+
+/*
+ * 判断buf是否为空
+ * */
+bool buf_empty(buf_t* buf);
+
+/*
+ * 扩展buf, len 必须大于 buf->cap;
+ * */
+int buf_resize(buf_t* buf, size_t len);
+
+/*
+ * 清除buf, 设置长度为0
+ *
+ * code:
+ *    int a[] = { 0x01, 0x05, 0x03 };
+ *    buf_t buf = buf_new(p(a), 10);
+ *
+ *    buf_clear(&buf);
+ *
+ *    // buf -> (0, 32) { };
+ * */
+void buf_clear(buf_t* buf);
+
+/*
+ * 释放buf
+ *
+ * code:
+ *    int a[] = { 0x01, 0x05, 0x03 };
+ *    buf_t buf = buf_new(p(a), 10);
+ *
+ *    buf_clear(&buf);
+ *
+ *    assert(nullptr == buf);
+ * */
+void buf_cleanup(buf_t* buf);
+
+/*
+ * 在buf结尾追加数据
+ *
+ * code:
+ *    int a[] = { 0x01, 0x05, 0x03 };
+ *    buf_t buf = buf_new(p(a), 10);
+ *
+ *    // buf -> (10, 32) { 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x03, 0x00};
+ *
+ *    int b[] = { 0x11, 0x3 };
+ *
+ *    buf_push(&buf, p(b), 6);
+ *
+ *    // buf -> (6, 32) { 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x03, 0x00, 0x11, 0x00,
+ *    0x00, 0x00, 0x03, 0x00};
+ *
+ * code:
+ *    int a[] = { 0x01, 0x05, 0x03 };
+ *    buf_t buf = buf_new(p(a), 10);
+ *
+ *    // buf -> (10, 32) { 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x03, 0x00};
+ *
+ *    buf_push(&buf, 0x0a);
+ *
+ *    // buf -> (14, 32) { 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x03, 0x00, 0x0a, 0x00,
+ *    0x00, 0x00};
+ * */
+int __buf_push(buf_t* buf, c_buf c_buffer, size_t size);
+#define buf_push(buf, c_buffer, arg...)                                                            \
+  ({                                                                                               \
+    typeof(c_buffer) _b_push__c_buf = (c_buffer);                                                  \
+    __buf_push(buf,                                                                                \
+               va_or((c_buf)&_b_push__c_buf, (c_buf)_b_push__c_buf, arg),                          \
+               va_0th(sizeof(typeof(c_buffer)), arg));                                             \
+  })
+
+/*
+ * 从buf结尾拷贝出一部分字符串, 并将长度 - len
+ *
+ * code:
+ *    int a[] = { 0x01, 0x05, 0x03 };
+ *    buf_t buf = buf_new(p(a), 10);
+ *
+ *    // buf -> (10, 32) { 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x03, 0x00};
+ *
+ *    int b[2] = {  };
+ *
+ *    buf_pop(&buf, p(b), 8);
+ *
+ *    // buf -> (2, 32) { 0x01, 0x00 };
+ *    // b   ->         { 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x03, 0x00 };
+ *
+ * code:
+ *    int a[] = { 0x01, 0x05, 0x03 };
+ *    buf_t buf = buf_new(p(a), 10);
+ *
+ *    // buf -> (10, 32) { 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x03, 0x00};
+ *
+ *    int b = 0;
+ *    buf_push(&buf, b);
+ *
+ *    // buf -> (6, 32) { 0x01, 0x00, 0x00, 0x00, 0x05, 0x00
+ *    // a   ->         { 0x00, 0x00, 0x03, 0x00 };
+ * */
+int __buf_pop(buf_t* buf, c_buf c_buffer, size_t size);
+#define buf_pop(buf, c_buffer, arg...)                                                             \
+  ({                                                                                               \
+    __buf_pop(buf,                                                                                 \
+              va_or((c_buf)&c_buffer, (c_buf)c_buffer, arg),                                       \
+              va_0th(sizeof(typeof(c_buffer)), arg));                                              \
+  })
+
+/*
+ * 与 `buf_pop` 类似, 不同的是长度不会发生改变
+ *
+ * code:
+ *    int a[] = { 0x01, 0x05, 0x03 };
+ *    buf_t buf = buf_new(p(a), 10);
+ *
+ *    // buf -> (10, 32) { 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x03, 0x00};
+ *
+ *    int b[2] = {  };
+ *
+ *    buf_pop(&buf, p(b), 8);
+ *
+ *    // buf -> (2, 32) { 0x01, 0x00 };
+ *    // b   ->         { 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x03, 0x00 };
+ *
+ * code:
+ *    int a[] = { 0x01, 0x05, 0x03 };
+ *    buf_t buf = buf_new(p(a), 10);
+ *
+ *    // buf -> (10, 32) { 0x01, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x03, 0x00};
+ *
+ *    int b = 0;
+ *    buf_pop(&buf, b);
+ *
+ *    // buf -> (6, 32) { 0x01, 0x00, 0x00, 0x00, 0x05, 0x00
+ *    // b   ->         { 0x00, 0x00, 0x03, 0x00 };
+ * */
+int __buf_peek(buf_t* buf, c_buf c_buffer, size_t size);
+#define buf_peek(buf, c_buffer, arg...)                                                            \
+  ({                                                                                               \
+    __buf_peek(buf,                                                                                \
+               va_or((c_buf)&c_buffer, (c_buf)c_buffer, arg),                                      \
+               va_0th(sizeof(typeof(c_buffer)), arg));                                             \
+  })
+
+#define buf_dis(buf) inf_hex((*(buf))->c_buf, (*(buf))->len)
