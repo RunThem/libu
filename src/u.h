8cb03b295188766c6d001aaea3501def265e76a5
RunThem <iccy.fun@outlook.com>
2023-06-08 12:13:39 +0800

    添加切片宏, 使用 `clang -E src/main.c` 来查看宏的效果

--- src/u.h
+++ src/u.h
@@ -25,12 +25,13 @@
 #define __va_size(_0, _1, _2, _3, _4, _5, _6, _n, ...) _n
 #define __va_opt(...)                                  __VA_OPT__(, )
 
-#define va_cat(x, y)   __va_cat(x, y)
-#define va_at(i, ...)  __va_at(i __VA_OPT__(, ) __VA_ARGS__)
-#define va_cut(i, ...) __va_cut(i __VA_OPT__(, ) __VA_ARGS__)
-#define va_size(...)   __va_size("ignore" __VA_OPT__(, ) __VA_ARGS__, 6, 5, 4, 3, 2, 1, 0)
-#define va_empty(...)  __va_size("ignore" __VA_OPT__(, ) __VA_ARGS__, 0, 0, 0, 0, 0, 0, 1)
-#define va_opt(i, ...) __va_opt(__va_cut(i, __VA_ARGS__))
+#define va_cat(x, y)     __va_cat(x, y)
+#define va_at(i, ...)    __va_at(i __VA_OPT__(, ) __VA_ARGS__)
+#define va_cut(i, ...)   __va_cut(i __VA_OPT__(, ) __VA_ARGS__)
+#define va_size(...)     __va_size("ignore" __VA_OPT__(, ) __VA_ARGS__, 6, 5, 4, 3, 2, 1, 0)
+#define va_empty(...)    __va_size("ignore" __VA_OPT__(, ) __VA_ARGS__, 0, 0, 0, 0, 0, 0, 1)
+#define va_opt(i, ...)   __va_opt(__va_cut(i, __VA_ARGS__))
+#define va_slice(i, ...) va_opt(i, __VA_ARGS__) va_cut(i, __VA_ARGS__)
 
 #define __va_th(...) va_at(0, __VA_ARGS__)
 
