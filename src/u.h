9c62be4054e7e3e7725d28d89b79cb4e74ca1562
RunThem <iccy.fun@outlook.com>
2023-06-08 12:10:45 +0800

    添加带有默认值的宏
    如:
    va_0th(100) -> 100
    va_0th(100, 1) -> 1
    va_0th(100, 1, 2) -> 1

--- src/u.h
+++ src/u.h
@@ -23,9 +23,20 @@
 #define __va_at(i, ...)                                __va_cat(__va_at_, i)(__VA_ARGS__)
 #define __va_cut(i, ...)                               __va_cat(__va_cut_, i)(__VA_ARGS__)
 #define __va_size(_0, _1, _2, _3, _4, _5, _6, _n, ...) _n
+#define __va_opt(...)                                  __VA_OPT__(, )
 
 #define va_cat(x, y)   __va_cat(x, y)
 #define va_at(i, ...)  __va_at(i __VA_OPT__(, ) __VA_ARGS__)
 #define va_cut(i, ...) __va_cut(i __VA_OPT__(, ) __VA_ARGS__)
 #define va_size(...)   __va_size("ignore" __VA_OPT__(, ) __VA_ARGS__, 6, 5, 4, 3, 2, 1, 0)
 #define va_empty(...)  __va_size("ignore" __VA_OPT__(, ) __VA_ARGS__, 0, 0, 0, 0, 0, 0, 1)
+#define va_opt(i, ...) __va_opt(__va_cut(i, __VA_ARGS__))
+
+#define __va_th(...) va_at(0, __VA_ARGS__)
+
+#define va_0th(def, ...) __va_th(va_cut(0, __VA_ARGS__) va_opt(0, __VA_ARGS__) def)
+#define va_1th(def, ...) __va_th(va_cut(1, __VA_ARGS__) va_opt(1, __VA_ARGS__) def)
+#define va_2th(def, ...) __va_th(va_cut(2, __VA_ARGS__) va_opt(2, __VA_ARGS__) def)
+#define va_3th(def, ...) __va_th(va_cut(3, __VA_ARGS__) va_opt(3, __VA_ARGS__) def)
+#define va_4th(def, ...) __va_th(va_cut(4, __VA_ARGS__) va_opt(4, __VA_ARGS__) def)
+#define va_5th(def, ...) __va_th(va_cut(5, __VA_ARGS__) va_opt(5, __VA_ARGS__) def)
