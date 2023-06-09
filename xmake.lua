283e91e644b07671c88247c077da6a2b8f5abbaa
RunThem <iccy.fun@outlook.com>
2023-06-09 14:23:03 +0800

    修正`*.in`忘记安装的问题

--- xmake.lua
+++ xmake.lua
@@ -22,6 +22,7 @@ target('u', function()
   set_kind('static')
   add_files('src/*.c')
   add_headerfiles('src/*.h', { prefixdir = 'u' })
+  add_headerfiles('src/*.in', { prefixdir = 'u' })
 
   add_packages('mimalloc')
 end)
