8213cd79adc8b72bd59836a5d10bd1c908526677
RunThem <iccy.fun@outlook.com>
2023-07-18 22:15:13 +0800

    使用一种新的方式写泛型数据结构

--- main.c
+++ main.c
@@ -1,24 +1,67 @@
 /* libs */
-#include "src/buf.h"
-#include "src/fs.h"
-#include "src/list.h"
-#include "src/map.h"
-#include "src/sock.h"
-#include "src/str.h"
+// #include "src/buf.h"
+// #include "src/fs.h"
+// #include "src/list.h"
+// #include "src/map.h"
+// #include "src/sock.h"
+// #include "src/str.h"
 #include "src/u.h"
-#include "src/vec.h"
+// #include "src/vec.h"
 
 #define _typeof(t) __builtin_classify_type(t)
 
-#define me(x, ...) (&(struct { typeof(x) _; }){(x)})
+typedef struct {
+  size_t itsize;
+  size_t len;
+  size_t cap;
+  any_t items;
+} vec_t;
 
-void print(void* ptr) {
-  inf("%d\n", *(int*)ptr);
+#undef vec
+#define vec(T)                                                                                     \
+  struct {                                                                                         \
+    vec_t _;                                                                                       \
+    T it;                                                                                          \
+  }
+
+void vec_init(any_t _self, size_t itsize, size_t cap) {
+  vec_t* self = as(_self, vec_t*);
+
+  self->itsize = itsize;
+  self->len    = 0;
+  self->cap    = cap;
+
+  self->items = u_calloc(cap, itsize);
+};
+
+void vec_push(any_t _self, any_t it) {
+  vec_t* self = as(_self, vec_t*);
+
+  memcpy(self->items + self->itsize * self->len, it, self->itsize);
+  self->len++;
 }
 
-int main(int argc, const char** argv) {
+void vec_pop(any_t _self) {
+  vec_t* self = as(_self, vec_t*);
 
-  print(me(10));
+  self->len--;
+  memcpy(_self + sizeof(vec_t), self->items + self->itsize * self->len, self->itsize);
+}
+
+void print(void* ptr) {
+  inf("%d", *as(ptr, int*));
+}
+
+struct st {
+  int a;
+  char b;
+  long d;
+};
+
+fn_eq_def(struct_st, struct st, (x.a == y.a));
+fn_cmp_def(struct_st, struct st, (x.a > y.a));
+
+int main(int argc, const char** argv) {
 
   return 0;
 }
