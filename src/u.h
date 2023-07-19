3fce92083f63cd51dfd8c8f6498cc74af11a20ae
RunThem <iccy.fun@outlook.com>
2023-07-19 23:43:05 +0800

    修正 u_ret_if() 宏

--- src/u.h
+++ src/u.h
@@ -125,14 +125,14 @@
 /* clang-format off */
 #define u_if(expr, arg...)              do { if (expr) { err("(%s) " va_0th("", arg), #expr va_slice(1, arg));                        } } while (0)
 #define u_die_if(expr, arg...)          do { if (expr) { err("(%s) " va_0th("", arg), #expr va_slice(1, arg)); exit(EXIT_FAILURE);    } } while (0)
-#define u_ret_if(expr, code, arg...)    do { if (expr) { err("(%s) " va_0th("", arg), #expr va_slice(1, arg)); return (code);         } } while (0)
+#define u_ret_if(expr, code, arg...)    do { if (expr) { err("(%s) " va_0th("", arg), #expr va_slice(1, arg)); return  code;          } } while (0)
 #define u_goto_if(expr, arg...)         do { if (expr) { err("(%s) " va_1th("", arg), #expr va_slice(2, arg)); goto va_0th(err, arg); } } while (0)
 #define u_free_if(mem)                  do { if ((mem) != nullptr) { u_free(mem);                                                     } } while (0)
 #define u_close_if(fd)                  do { if ((fd) >= 0) { close(fd);                                                              } } while (0)
 #define u_fclose_if(fp)                 do { if ((fp) != nullptr) { fclose(fd);                                                       } } while (0)
 
-#define u_alloc_if(mem, arg...)         do { u_goto_if((mem) == nullptr, err, arg)                                                      } while (0)
-#define u_ret_no_if(expr, arg...)       do { u_ret_if(expr, , arg)                                                                      } while (0)
+#define u_alloc_if(mem, arg...)         u_goto_if((mem) == nullptr, err, arg)
+#define u_ret_no_if(expr, arg...)       u_ret_if(expr, , arg)
 /* clang-format on */
 
 /*************************************************************************************************
