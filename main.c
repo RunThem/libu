63ba80570a2afce75f075963dae77fcdcf5ad06a
RunThem <iccy.fun@outlook.com>
2023-08-04 16:45:21 +0800

    添加两个打印宏, 可以在 release 模式下使用

--- main.c
+++ main.c
@@ -14,24 +14,7 @@
 
 int main(int argc, const char** argv) {
 
-  map(int, char) mm = {};
-
-  map_init(&mm);
-
-  map_push(&mm, 12, 'a');
-  map_push(&mm, 2, '4');
-  map_push(&mm, 14, 'v');
-  map_push(&mm, 132, 'm');
-
-  printf("len is %ld\n", map_len(&mm));
-  printf("'%c'\n", map_at(&mm, 12));
-  printf("'%c'\n", map_at(&mm, 2));
-  printf("'%c'\n", map_at(&mm, 14));
-  printf("'%c'\n", map_at(&mm, 132));
-
-  map_for(&mm, i) {
-    printf("%d, %c\n", mm.key, mm.val);
-  }
+  println("Hello libu!");
 
   return 0;
 }
