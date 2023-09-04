d1a063e7ddd77685804ab1f9e0c20f6e4050f256
RunThem <iccy.fun@outlook.com>
2023-09-04 22:37:04 +0800

    泛型 avl tree 的简单用例

--- main.c
+++ main.c
@@ -8,8 +8,10 @@
 // #include "stack.h"
 // #include "str.h"
 #include "u.h"
-// #include "vec.h"
+#include "vec.h"
 // #include "obj.h"
+#include "avl.h"
+// #include "stack.h"
 
 // #include <backtrace-supported.h>
 // #include <backtrace.h>
@@ -24,67 +26,36 @@ void boo() {
   // backtrace_print((struct backtrace_state*)__bt_state, 0, stderr);
 }
 
-#include "avl.h"
+struct st_t {
+  int a;
+  char c;
+};
+
+int st_cmp(const void* a, const void* b) {
+  struct st_t* _a = as(a, struct st_t*);
+  struct st_t* _b = as(b, struct st_t*);
+
+  if (_a->a < _b->a) {
+    return -1;
+  } else if (_a->a > _b->a) {
+    return 1;
+  }
+
+  return 0;
+}
+
 int main(int argc, const char** argv) {
   // __bt_state = backtrace_create_state(argv[1], 0, nullptr, nullptr);
 
-  avl(int) t = nullptr;
+  avl(struct st_t) t = avl_new(struct st_t, st_cmp);
 
-  t = avl_new(int, fn_cmp_use(int32));
+  struct st_t st = {.a = 23, .c = 'e'};
 
-  avl_push(t, 4);
-  avl_push(t, 87);
-  avl_push(t, 23);
-  avl_push(t, 39);
-  avl_push(t, 46);
-  avl_push(t, 35);
-  avl_push(t, 31);
-  avl_push(t, 56);
-  avl_push(t, 30);
-  avl_push(t, 21);
-  avl_push(t, 25);
-  avl_push(t, 72);
-  avl_push(t, 65);
-  avl_push(t, 54);
-  avl_push(t, 44);
-  avl_push(t, 80);
-  avl_push(t, 55);
-  avl_push(t, 18);
-  avl_push(t, 28);
-  avl_push(t, 34);
-  avl_push(t, 6);
-  avl_push(t, 33);
-  avl_push(t, 82);
+  avl_push(t, st);
 
-  avl_debug(t);
+  struct st_t s = {.a = 23};
 
-  avl_clear(t);
-
-#if 0
-  avl_pop(t, 4);
-  avl_pop(t, 87);
-  avl_pop(t, 23);
-  avl_pop(t, 39);
-  avl_pop(t, 46);
-  avl_pop(t, 35);
-  avl_pop(t, 31);
-  avl_pop(t, 56);
-  avl_pop(t, 30);
-  avl_pop(t, 21);
-  avl_pop(t, 25);
-  avl_pop(t, 72);
-  avl_pop(t, 65);
-  avl_pop(t, 54);
-  avl_pop(t, 44);
-  avl_pop(t, 80);
-  avl_pop(t, 55);
-  avl_pop(t, 18);
-  avl_pop(t, 28);
-  avl_pop(t, 34);
-  avl_pop(t, 6);
-  avl_pop(t, 33);
-  avl_pop(t, 82);
-#endif
+  inf("%c", avl_at(t, s).c);
 
   return 0;
 }
