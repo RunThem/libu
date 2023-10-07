e225726aa48e14e243fd9807316ce63b498da98e
RunThem <iccy.fun@outlook.com>
2023-10-07 21:51:06 +0800

    更新泛型 vec 单元测试

--- dev/check/vec.c
+++ dev/check/vec.c
@@ -52,29 +52,36 @@ mut_test(vec_interface) {
 
   mut_assert(true == u_vec_empty(v));
 
+  /* 31, 30, ..., 1, 0 */
   each(i, N) {
     u_vec_push_front(v, i);
   }
 
+  /* <- 0, 1, ..., 30, 31 */
   each(i, N) {
     u_vec_push_back(v, i);
   }
 
+  /* <- 0, 1, ..., 30, 31 -> */
   each(i, N) {
     u_vec_push(v, N + i, i);
   }
 
+  /* <- 31, 30, ..., 1, 0 -> */
   each(i, N) {
     u_vec_push(v, N * 2, i);
   }
 
-  /* 31, 30, ..., 1, 0, 0, 1, ..., 30, 31, 31, 30, ..., 1, 0, 0, 1, ..., 30, 31 */
+  /* [       0       ]  [       2       ]  [        3      ]  [       1       ]
+   * 31, 30, ..., 1, 0, 0, 1, ..., 30, 31, 31, 30, ..., 1, 0, 0, 1, ..., 30, 31
+   * */
 
   mut_assert(false == u_vec_empty(v));
   mut_assert(N * 4 == u_vec_len(v));
 
   mut_assert(N - 1 == u_vec_at_front(v));
   mut_assert(N - 1 == u_vec_at_back(v));
+  printf("%d", u_vec_at_back(v));
 
   each(i, N) {
     mut_assert(N - 1 - i == u_vec_at(v, i));
@@ -110,6 +117,25 @@ mut_test(vec_interface) {
     mut_assert(i == u_vec_at(v, i));
   }
 
+  u_vec_clear(v);
+
+  mut_assert(0 == u_vec_len(v));
+
+  each(i, 4) {
+    u_vec_push_back(v, i);
+  }
+
+  u_vec_re_front(v, 3);
+  u_vec_re_back(v, 0);
+  u_vec_re(v, 1, 2);
+  u_vec_re(v, 2, 1);
+
+  mut_assert(4 == u_vec_len(v));
+
+  each(i, 4) {
+    mut_assert(4 - 1 - i == u_vec_at(v, i));
+  }
+
   u_vec_cleanup(v);
 }
 
@@ -124,11 +150,11 @@ mut_test(vec_iterator) {
   }
 
   u_vec_for(v, i) {
-    mut_assert(i == *v->item);
+    mut_assert(i == v->item);
   }
 
   u_vec_rfor(v, i) {
-    mut_assert(i == *v->item);
+    mut_assert(i == v->item);
   }
 
   u_vec_cleanup(v);
