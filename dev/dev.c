9a4e0628a0247e3d8dbba894f3d5c2c0d43d2c77
RunThem <iccy.fun@outlook.com>
2023-12-11 20:33:46 +0800

    尝试实现预处理器

--- dev/dev.c
+++ dev/dev.c
@@ -52,41 +52,34 @@ typedef struct {
   int a;
   char c;
 } st_t;
-
-fn_compe_def(st_t, (x.a == y.a), (x.a > y.a));
+fn_compe_def(st_t, x.a == y.a, x.a > y.a);
 
 #define to_str(...)   #__VA_ARGS__
 #define $(shell, ...) printf("{" to_str(shell) "}\n" va_opt(, ) __VA_ARGS__)
 
-int lsexec2(int* in, int* out, char* path, char* arg[]) {
-  int fd_i[2];
-  int fd_o[2];
-  char buf[BUFSIZ];
-  int len = 0;
+ret_t code = 0;
 
-  // 创建两个管道，fd_i用于接管子进程的标准输入，fd_o用于标准输出
-  pipe(fd_i);
-  pipe(fd_o);
+#undef $
+#define $(...)
 
-  pid_t p = fork();
-  if (p < 0)
-    return -1;
-  if (p > 0) {       // parent
-    close(fd_i[0]);  // 关闭输入管道的读端
-    *in = fd_i[1];   // 将写端暴露给调用者，这样调用者可以给子进程发送命令
-    close(fd_o[1]);  // 关闭输出管道的写端
-    *out = fd_o[0];  // 将读端暴露给调用者，这样调用者可以读取子进程的输出
-    return 0;
-  } else {           // child
-    close(fd_i[1]);  // 关闭输入管道的写端
-    dup2(fd_i[0],
-         STDIN_FILENO);  // 将标准输入复制到输入管道的读端，这样就可以把父进程的输入转接到标准输入
-    close(fd_o[0]);      // 关闭输出管道的读端
-    dup2(fd_o[1], STDOUT_FILENO);
+#define u_vec(T) T*
+#define u_push(self, ...)
+#define u_pop(self, ...)
+#define u_map(self, ...)
+#define u_filter(self, ...) (void)self
+#define u_clone(self)
+#define u_at(self, idx) (*(self))
+#define u_size(self)    (sizeof(*(self)))
+#define u_erase(self, ...)
+#define u_cleanup(self)
+#define u_clear(self)
+#define u_dump(self)
 
-    // 将标准输出复制到输出管道的写端，这样父进程就可以通过读端读取标准输出的内容执行需要的命令
-    return execv(path, arg);
-  }
+#define fn(func, ret, ...) ret func(__VA_ARGS__)
+
+fn(hello_num, int, int a) {
+
+  return 1;
 }
 
 int main(int argc, const char** argv) {
@@ -94,53 +87,42 @@ int main(int argc, const char** argv) {
 
   infln("hello libu!");
 
-  // u_str_t s = "print";
+  // $(Debug, Clone)
+  struct {
+    int a;
+    char c;
+
+    int (*get)();
+  } st;
+
+  u_vec(int) v = {};
+
+  u_push(v, 1, 2, 3, 4, 5);
+
+  u_map(v, [int a, void c] { return a * 2; });
+
+  int a;
+  u_filter(v, [int a, &l] bool { return a % l == 0 : true : false; });
+
+  hello_num(u_at(v, 0));
+  // hello_num(v.at(0));
+
+#if 0
+  u_str_t s = "print";
   /* clang-format off */
-  // $((cat main.c - c,n,e | grep "%s" | %s), s, s);
+  $((cat main.c - c,n,e | grep "%s" | %s), s, s);
   /* clang-format on */
 
-  // int $$ = 0;
+  int $$     = 0;
 
-  // int in;
-  // int out;
+  char buf[1024] = {0};
 
-  // char* args[] = {"ncat", "-u", "localhost", "8080", NULL};
-  // char* args[] = {"lua", NULL};
-  // char* args[] = {"python", NULL};
+  FILE* fd = popen("ls", "r");
+  u_die_if(fd == nullptr);
 
-  // execv("/usr/bin/lua", args);
+  u_str_t result = u_str_new(100);
 
-#if 1
-  // int ret = lsexec2(&in, &out, "/usr/bin/ncat", args);
-  // int ret = lsexec2(&in, &out, "/usr/bin/lua", args);
-  // int ret = lsexec2(&in, &out, "/home/coder/.local/bin/python", args);
-  // u_die_if(ret == -1);
-
-  // infln("ret is %d", ret);
-
-  // size_t len     = 0;
-  // char buf[1024] = {};
-
-  // sleep(1);
-
-  // len = read(out, buf, arr_len(buf));
-  // infln("len is %zu, %s", len, buf);
-
-  // while (true) {
-  // len = read(out, buf, arr_len(buf));
-  // infln("len is %zu, buf is %s", len, buf);
-
-  // scanf("%s", buf);
-  // strcat(buf, "print(\"heljas\")\r\n");
-  // write(in, buf, strlen(buf));
-
-  // usleep(10000);
-
-  // bzero(buf, arr_len(buf));
-
-  // len = read(out, buf, arr_len(buf));
-  // infln("len is %zu, buf is %s", len, buf);
-  // }
+  infln("%s", buf);
 #endif
 
   return EXIT_SUCCESS;
