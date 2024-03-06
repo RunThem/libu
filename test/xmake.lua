76fb9e6079d2a24b0519fab2bbd77e8b353eb1aa
RunThem <iccy.fun@outlook.com>
2024-03-06 17:42:15 +0800

    不要其他的测试框架了

--- test/xmake.lua
+++
@@ -1,7 +0,0 @@
-target('codegen', function()
-  set_kind('phony')
-  -- add_files('main.c')
-  -- set_default('false')
-
-  add_configfiles('main.c.in', { variables = { funcs = "{ 'vec_new', test_vec_new }," } })
-end)
