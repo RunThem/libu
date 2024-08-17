1ea3d46c2dad22044c62b4528f94f8ceba443d08
RunThem <iccy.fun@outlook.com>
2024-08-17 10:28:52 +0800

    test: add unit test

---
+++ tests/test_str_base.c
@@ -0,0 +1,41 @@
+#include <u/u.h>
+
+test() {
+  u_str_t s = nullptr;
+
+  /*  */
+  s = u_str_t();
+
+  mut_n(s);
+  mut_n(s->ptr);
+  mut_e(0, s->len);
+
+  u_str_cleanup(s);
+  mut_nn(s);
+
+  s = u_str_t('h');
+  mut_n(s);
+  mut_n(s->ptr);
+  mut_e(1, s->len);
+  mut_t(0 == strcmp(s->ptr, "h"));
+  u_str_cleanup(s);
+
+  /*  */
+  s = u_str_t("hello");
+  mut_n(s);
+  mut_n(s->ptr);
+  mut_e(5, s->len);
+  mut_t(0 == strcmp(s->ptr, "hello"));
+  u_str_cleanup(s);
+
+  /*  */
+  s = u_str_t("h%so", "ell");
+  mut_n(s);
+  mut_n(s->ptr);
+  mut_e(5, s->len);
+  mut_t(0 == strcmp(s->ptr, "hello"));
+  u_str_cleanup(s);
+
+  s = u_str_t("l");
+  u_str_cat(s, 'o');
+}
