3357ab1fe59fa90c571defdc08faac17e79f1331
iccy <iccy.fun@outlook.com>
2025-11-27 12:05:09 +0000

    feat(chibicc): Add unary plus and minus

--- chibicc/main.c
+++ chibicc/main.c
@@ -129,6 +129,7 @@ typedef enum {
   ND_SUB,  // -
   ND_MUL,  // *
   ND_DIV,  // /
+  ND_NEG,  // unary -
   ND_NUM,  // 数字
 } node_kind_e;
 
@@ -148,12 +149,17 @@ pri node_mut_t new_binary(node_kind_e kind, node_mut_t lhs, node_mut_t rhs) {
   return new (node_t, .kind = kind, .lhs = lhs, .rhs = rhs);
 }
 
+pri node_mut_t new_unary(node_kind_e kind, node_mut_t expr) {
+  return new (node_t, .kind = kind, .lhs = expr);
+}
+
 pri node_mut_t new_number(int val) {
   return new (node_t, .kind = ND_NUM, .val = val);
 }
 
 pri node_mut_t expr(token_mut_t* rest, token_mut_t tok);
 pri node_mut_t mul(token_mut_t* rest, token_mut_t tok);
+pri node_mut_t unary(token_mut_t* rest, token_mut_t tok);
 pri node_mut_t primary(token_mut_t* rest, token_mut_t tok);
 
 /// expr = mul ("+" mul | "-" mul)*
@@ -177,18 +183,18 @@ pri node_mut_t expr(token_mut_t* rest, token_mut_t tok) {
   }
 }
 
-/// mul = primary ("*" primary | "/" primary)*
+/// mul = unary ("*" unary | "/" primary)*
 pri node_mut_t mul(token_mut_t* rest, token_mut_t tok) {
-  node_mut_t node = primary(&tok, tok);
+  node_mut_t node = unary(&tok, tok);
 
   for (;;) {
     if (equal(tok, "*")) {
-      node = new_binary(ND_MUL, node, primary(&tok, tok->next));
+      node = new_binary(ND_MUL, node, unary(&tok, tok->next));
       continue;
     }
 
     if (equal(tok, "/")) {
-      node = new_binary(ND_DIV, node, primary(&tok, tok->next));
+      node = new_binary(ND_DIV, node, unary(&tok, tok->next));
       continue;
     }
 
@@ -196,6 +202,20 @@ pri node_mut_t mul(token_mut_t* rest, token_mut_t tok) {
 
     return node;
   }
+}
+
+/// unary = ("+" | "-") unary
+///       | primary
+pri node_mut_t unary(token_mut_t* rest, token_mut_t tok) {
+  if (equal(tok, "+")) {
+    return unary(rest, tok->next);
+  }
+
+  if (equal(tok, "-")) {
+    return new_unary(ND_NEG, unary(rest, tok->next));
+  }
+
+  return primary(rest, tok);
 }
 
 /// primary = "(" expr ")" | num
@@ -234,9 +254,14 @@ pri void pop(char* arg) {
 }
 
 pri void gen_expr(node_ref_t node) {
-  if (node->kind == ND_NUM) {
-    printf("  mov $%d, %%rax\n", node->val);
-    return;
+  switch (node->kind) {
+    case ND_NUM: printf("  mov $%d, %%rax\n", node->val); return;
+    case ND_NEG:
+      gen_expr(node->lhs);
+      printf("  neg %%rax\n");
+      return;
+
+    default: break;
   }
 
   gen_expr(node->rhs);
