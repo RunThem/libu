06c6b5a6297c1b1d6b5f6f83cd3e08e30cbed424
iccy <iccy.fun@outlook.com>
2025-12-19 07:10:56 +0000

    refactor(chibicc) no functionality change

--- chibicc/codegen.c
+++ chibicc/codegen.c
@@ -10,18 +10,26 @@ pri ObjMut_t current_fn = nullptr;
 pri void gen_expr(NodeMut_t node);
 pri void gen_stmt(NodeMut_t node);
 
+pri void println(char* fmt, ...) {
+  va_list ap = {};
+  va_start(ap, fmt);
+  vprintf(fmt, ap);
+  va_end(ap);
+
+  printf("\n");
+}
 pri int count() {
   pri int i = 1;
   return i++;
 }
 
 pri void push() {
-  printf("  push %%rax\n");
+  println("  push %%rax");
   depth++;
 }
 
 pri void pop(char* arg) {
-  printf("  pop %s\n", arg);
+  println("  pop %s", arg);
   depth--;
 }
 
@@ -31,10 +39,10 @@ pri void gen_addr(NodeRef_t node) {
     case ND_VAR:
       if (node->var->is_local) {
         // 局部变量
-        printf("  lea %d(%%rbp), %%rax\n", node->var->offset);
+        println("  lea %d(%%rbp), %%rax", node->var->offset);
       } else {
         // 全局变量
-        printf("  lea %s(%%rip), %%rax\n", node->var->name);
+        println("  lea %s(%%rip), %%rax", node->var->name);
       }
       return;
 
@@ -62,9 +70,9 @@ pri void load(TypeMut_t ty) {
   }
 
   if (ty->size == 1) {
-    printf("  movsbq (%%rax), %%rax\n");
+    println("  movsbq (%%rax), %%rax");
   } else {
-    printf("  mov (%%rax), %%rax\n");
+    println("  mov (%%rax), %%rax");
   }
 }
 
@@ -73,19 +81,19 @@ pri void store(TypeMut_t ty) {
   pop("%rdi");
 
   if (ty->size == 1) {
-    printf("  mov %%al, (%%rdi)\n");
+    println("  mov %%al, (%%rdi)");
   } else {
-    printf("  mov %%rax, (%%rdi)\n");
+    println("  mov %%rax, (%%rdi)");
   }
 }
 
 /// 为给定节点生成代码
 pri void gen_expr(NodeMut_t node) {
   switch (node->kind) {
-    case ND_NUM: printf("  mov $%d, %%rax\n", node->val); return;
+    case ND_NUM: println("  mov $%d, %%rax", node->val); return;
     case ND_NEG:
       gen_expr(node->lhs);
-      printf("  neg %%rax\n");
+      println("  neg %%rax");
       return;
     case ND_VAR:
       gen_addr(node);
@@ -120,8 +128,8 @@ pri void gen_expr(NodeMut_t node) {
         pop(argreg64[i]);
       }
 
-      printf("  mov $0, %%rax\n");
-      printf("  call %s\n", node->funcname);
+      println("  mov $0, %%rax");
+      println("  call %s", node->funcname);
       return;
     }
 
@@ -134,30 +142,30 @@ pri void gen_expr(NodeMut_t node) {
   pop("%rdi");
 
   switch (node->kind) {
-    case ND_ADD: printf("  add %%rdi, %%rax\n"); return;
-    case ND_SUB: printf("  sub %%rdi, %%rax\n"); return;
-    case ND_MUL: printf("  imul %%rdi, %%rax\n"); return;
+    case ND_ADD: println("  add %%rdi, %%rax"); return;
+    case ND_SUB: println("  sub %%rdi, %%rax"); return;
+    case ND_MUL: println("  imul %%rdi, %%rax"); return;
     case ND_DIV:
-      printf("  cqo\n");
-      printf("  idiv %%rdi\n");
+      println("  cqo");
+      println("  idiv %%rdi");
       return;
     case ND_EQ:
     case ND_NE:
     case ND_LT:
     case ND_LE:
-      printf("  cmp %%rdi, %%rax\n");
+      println("  cmp %%rdi, %%rax");
 
       if (node->kind == ND_EQ) {
-        printf("  sete %%al\n");
+        println("  sete %%al");
       } else if (node->kind == ND_NE) {
-        printf("  setne %%al\n");
+        println("  setne %%al");
       } else if (node->kind == ND_LT) {
-        printf("  setl %%al\n");
+        println("  setl %%al");
       } else if (node->kind == ND_LE) {
-        printf("  setle %%al\n");
+        println("  setle %%al");
       }
 
-      printf("  movzb %%al, %%rax\n");
+      println("  movzb %%al, %%rax");
       return;
 
     default: break;
@@ -172,15 +180,15 @@ pri void gen_stmt(NodeMut_t node) {
 
       int c = count();
       gen_expr(node->cond);
-      printf("  cmp $0, %%rax\n");
-      printf("  je .L.else.%d\n", c);
+      println("  cmp $0, %%rax");
+      println("  je .L.else.%d", c);
       gen_stmt(node->then);
-      printf("  jmp .L.end.%d\n", c);
-      printf(".L.else.%d:\n", c);
+      println("  jmp .L.end.%d", c);
+      println(".L.else.%d:", c);
       if (node->els) {
         gen_stmt(node->els);
       }
-      printf(".L.end.%d:\n", c);
+      println(".L.end.%d:", c);
       return;
     }
     case ND_FOR: {
@@ -189,18 +197,18 @@ pri void gen_stmt(NodeMut_t node) {
         gen_stmt(node->init);
       }
 
-      printf(".L.begin.%d:\n", c);
+      println(".L.begin.%d:", c);
       if (node->cond) {
         gen_expr(node->cond);
-        printf("  cmp $0, %%rax\n");
-        printf("  je .L.end.%d\n", c);
+        println("  cmp $0, %%rax");
+        println("  je .L.end.%d", c);
       }
       gen_stmt(node->then);
       if (node->inc) {
         gen_expr(node->inc);
       }
-      printf("  jmp .L.begin.%d\n", c);
-      printf(".L.end.%d:\n", c);
+      println("  jmp .L.begin.%d", c);
+      println(".L.end.%d:", c);
       return;
     }
     case ND_BLOCK:
@@ -210,7 +218,7 @@ pri void gen_stmt(NodeMut_t node) {
       return;
     case ND_RETURN:
       gen_expr(node->lhs);
-      printf("  jmp .L.return.%s\n", current_fn->name);
+      println("  jmp .L.return.%s", current_fn->name);
       return;
     case ND_EXPR_STMT: gen_expr(node->lhs); return;
 
@@ -239,16 +247,16 @@ pri void emit_data(ObjMut_t prog) {
   for (ObjMut_t var = prog; var; var = var->next) {
     u_cnt_if(var->is_function);
 
-    printf("  .data\n");
-    printf("  .globl %s\n", var->name);
-    printf("%s:\n", var->name);
+    println("  .data");
+    println("  .globl %s", var->name);
+    println("%s:", var->name);
 
     if (var->init_data) {
       for (int i = 0; i < var->ty->size; i++) {
-        printf("  .byte %d\n", var->init_data[i]);
+        println("  .byte %d", var->init_data[i]);
       }
     } else {
-      printf("  .zero %d\n", var->ty->size);
+      println("  .zero %d", var->ty->size);
     }
   }
 }
@@ -257,22 +265,22 @@ pri void emit_text(ObjMut_t prog) {
   for (ObjMut_t fn = prog; fn; fn = fn->next) {
     u_cnt_if(!fn->is_function);
 
-    printf("  .globl %s\n", fn->name);
-    printf("  .text\n");
-    printf("%s:\n", fn->name);
+    println("  .globl %s", fn->name);
+    println("  .text");
+    println("%s:", fn->name);
     current_fn = fn;
 
     // Prologue
-    printf("  push %%rbp\n");
-    printf("  mov %%rsp, %%rbp\n");
-    printf("  sub $%d, %%rsp\n", fn->stack_size);
+    println("  push %%rbp");
+    println("  mov %%rsp, %%rbp");
+    println("  sub $%d, %%rsp", fn->stack_size);
 
     int i = 0;
     for (ObjMut_t var = fn->params; var; var = var->next) {
       if (var->ty->size == 1) {
-        printf("  mov %s, %d(%%rbp)\n", argreg8[i++], var->offset);
+        println("  mov %s, %d(%%rbp)", argreg8[i++], var->offset);
       } else {
-        printf("  mov %s, %d(%%rbp)\n", argreg64[i++], var->offset);
+        println("  mov %s, %d(%%rbp)", argreg64[i++], var->offset);
       }
     }
 
@@ -281,10 +289,10 @@ pri void emit_text(ObjMut_t prog) {
     assert(depth == 0);
 
     // Epilogue
-    printf(".L.return.%s:\n", fn->name);
-    printf("  mov %%rbp, %%rsp\n");
-    printf("  pop %%rbp\n");
-    printf("  ret\n");
+    println(".L.return.%s:", fn->name);
+    println("  mov %%rbp, %%rsp");
+    println("  pop %%rbp");
+    println("  ret");
   }
 }
 
